b93a2d57f9f18b80173c09c3b49d20dc54c14b68
chris <chrisshields88@gmail.com>
2016-10-27 20:07:58 -0400

    Initial File Upload
    Initial upload
    Included:
    -Assets(Images, resume)
    -HTML Files, CSS, JS

---
+++ app.js
@@ -0,0 +1,18 @@
+(function(){
+	var app = angular.module('portfolio', [ ]);
+
+	app.controller('ContentController', function(){
+		
+	});
+
+	var about = {
+		img: ,
+
+	}
+})()
+
+
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
