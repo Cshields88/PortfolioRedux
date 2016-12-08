51eb118a9b97aab7d3112c93d0baf34f7b19e10d
chris <chrisshields88@gmail.com>
2016-12-08 16:58:15 -0500

    Update
    -- Updated and Added Files
    -- Added AngularJS to make a SPA instead of a static website

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-(function(){
-	var app = angular.module('portfolio', [ ]);
+var app = angular.module('portfolio', ['ngRoute']);
 
-	app.controller('ContentController', function(){
-		
-	});
+// Router
+app.config(function($routeProvider){
+  $routeProvider
 
-	var about = {
-		img: ,
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'HomeCTRL'
+  })
+  
+  .when('/portfolio', {
+    templateUrl: 'pages/portfolio.html',
+    controller: 'PortfolioCTRL'
+  })
+  
+  .when('/about', {
+    templateUrl: 'pages/about.html',
+    controller: 'AboutCTRL'
+  })
 
-	}
-})()
+  .when('/contact', {
+    templateUrl: 'pages/contact.html',
+    controller: 'ContactCTRL'
+  })
 
+  .otherwise({redirectTo: '/'});  
+});
 
-$(document).ready(function(){
-    // Initialize Tooltip
-    $('[data-toggle="tooltip"]').tooltip(); 
-})
+// Controllers
+
+app.controller('HomeCTRL', function($scope){
+  $scope.message = 'Hello From Home Controller';
+});
+
+app.controller('PortfolioCTRL', function($scope){
+  $scope.message = 'Hello From Portfolio Controller';
+});
+
+app.controller('AboutCTRL', function($scope){
+  $scope.message = 'Hello From About Controller';
+});
+app.controller('ContactCTRL', function($scope){
+  $scope.message = 'Hello From Contact Controller';
+});
